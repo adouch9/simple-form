e39e80a33850f857d8cfac50851f74173fa4eb97
adouch9 <mr.ouldmoussa@gmail.com>
2022-11-03 16:17:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Title from "./components/Title";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+
+  const handleSubmit = (event) => {
+    // J'empêche  le rafraichissement
+    event.preventDefault();
+    if (password !== confirm) {
+      alert(`Les mot de passe sont différent `);
+    }
+  };
+
+  const handleClick = () => {
+    if (password !== confirm) {
+      alert(`Les mot de passe sont différent `);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Title />
+      <form onSubmit={handleSubmit}>
+        <span>Name</span>
+        <input
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          type="text"
+          placeholder="Nom"
+          value={name}
+        ></input>
+        <span>Email</span>
+        <input
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          type="email"
+          placeholder="mail@mail.com"
+          value={email}
+          name="email"
+        ></input>
+        <span>Password</span>
+        <input
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          type="password"
+          placeholder="azerty"
+        ></input>
+        <span>Confirm your password</span>
+        <input
+          onChange={(event) => {
+            setConfirm(event.target.value);
+          }}
+          type="password"
+          placeholder="azerty"
+        ></input>
+        <input
+          onClick={() => {
+            handleClick("Cliqué !");
+          }}
+          type="submit"
+        ></input>
+      </form>
+      
+      <div>{email}</div>
+      <div>{name}</div>
+      <div>{password}</div>
+      <div>{confirm}</div>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+const Title = (props) => {
+  return (
+    <div>
+      <h1 id="title">Create account</h1>
+    </div>
+  );
+};
+
+export default Title;
